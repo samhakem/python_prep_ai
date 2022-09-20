6d5df7b9be58ea3de4b144e05c59149c1522d3d9
sam <samhakem@gmail.com>
2022-09-20 12:27:46 +0200

    Second commit - day two - python prep class.

---
+++ input_and_output.py
@@ -0,0 +1,21 @@
+# Input and Output
+
+# Output
+
+# ■ We often want an algorithm to give feedback to a human
+# ■ Use the built-in function print()
+# ■ Python offers lots of useful built-in functions
+# (see https://docs.python.org/3/library/functions.html#print)
+
+x = 0.123456789
+print("The value of x is", x)
+# The value of x is 0.123456789
+
+var = 10
+print(var)
+
+print('Value of var: ', var)
+
+print('Hello', 'Delineator', sep='_')
+
+print('Hello', 'Line', 'Break', 'My Old', 'Friend', sep='\n')
