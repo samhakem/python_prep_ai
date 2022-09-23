f11bb0c4a070e7f41a0bfc1d506e9b01f2c5d298
sam <samhakem@gmail.com>
2022-09-23 14:20:16 +0200

    Codewars kata collection commits - these will grow over time

--- katas.py
+++ katas.py
@@ -1,3 +1,12 @@
+# Invert values
+# Given a set of numbers, return the additive inverse of each.
+# Each positive becomes negatives, and the negatives become positives.
+# You can assume that all values are integers. Do not mutate the input array/list.
+
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+
 def invert(lst):
     """
     Step 1: given a set of numbers, iterate over each in the set
